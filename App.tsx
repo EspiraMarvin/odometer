3528e262e4f97562e2a147d8903ea2844d30beaa
EspiraMarvin <espiramarvin@gmail.com>
2024-11-21 23:40:15 +0300

    feat: watch gps location actively

--- App.tsx
+++ App.tsx
@@ -3,33 +3,34 @@ import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
-  Button,
   StyleSheet,
   Alert,
-  PermissionsAndroid,
-  Platform,
   TextInput,
+  Pressable,
+  TouchableOpacity,
+  Platform,
+  PermissionsAndroid,
 } from "react-native";
-import Geolocation from "react-native-geolocation-service";
+import * as Location from "expo-location";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
-  const [speed, setSpeed] = useState<number>(0); // Speed in kph
+  const [speed, setSpeed] = useState<number>(0); // Speed in kph/km/h
   const [tracking, setTracking] = useState(false);
+  const [pause, setPause] = useState(false);
   const [maxSpeedReached, setMaxSpeedReached] = useState(false);
 
   const [maxSpeed, setMaxSpeed] = useState(60); // Default max speed
   const [listening, setListening] = useState(false);
-  const [inputSpeed, setInputSpeed] = useState(""); // Input field value
+  const [inputSpeed, setInputSpeed] = useState(""); // Input field value for max speed
+
+  let subscription: { remove: any } | null = null;
 
   useEffect(() => {
-    if (speed > maxSpeed) {
-      setMaxSpeedReached(true);
-    }
+    setMaxSpeedReached(speed > maxSpeed);
   }, [speed, maxSpeed]);
 
   useEffect(() => {
-    let watchId: number | null = null;
-
     const requestPermissions = async () => {
       if (Platform.OS === "android") {
         const granted = await PermissionsAndroid.request(
@@ -40,7 +41,7 @@ export default function App() {
       return true;
     };
 
-    const startTracking = async () => {
+    const watchLocation = async () => {
       const hasPermission = await requestPermissions();
       if (!hasPermission) {
         Alert.alert(
@@ -50,44 +51,57 @@ export default function App() {
         return;
       }
 
-      watchId = Geolocation.watchPosition(
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      // actively watch your location
+      subscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 1000,
+          distanceInterval: 1,
+        },
         (position) => {
           const { speed } = position.coords;
-          console.log("SPEEED", speed);
-
-          // Convert m/s to kph and ensure it's a number
           setSpeed(speed ? parseFloat((speed * 3.6).toFixed(2)) : 0);
-        },
-        (error) => {
-          console.error(error);
-          Alert.alert("Error", "Unable to fetch location.");
-        },
-        {
-          enableHighAccuracy: true,
-          distanceFilter: 1, // Minimum distance (in meters) to trigger an update
-          interval: 1000, // Update every second
-          fastestInterval: 500,
         }
       );
     };
 
-    // if (speed > maxSpeed) {
-    //   setMaxSpeedReached(true);
-    // }
+    if (tracking && !pause) {
+      watchLocation();
+    }
 
-    if (tracking) {
-      startTracking();
-    } else if (watchId) {
-      Geolocation.clearWatch(watchId);
+    if (!tracking && !pause) {
+      setSpeed(0);
+      setMaxSpeedReached(false);
+
+      // Stop watching the location
+      if (subscription) {
+        subscription.remove();
+        subscription = null;
+      }
+    }
+
+    if (pause) {
+      setPause(true);
     }
 
     return () => {
-      if (watchId) Geolocation.clearWatch(watchId);
+      if (subscription) {
+        subscription.remove();
+        subscription = null;
+      }
     };
-  }, [tracking]);
+  }, [tracking, pause]);
 
   const handleSetSpeed = () => {
     const parsedSpeed = parseInt(inputSpeed, 10);
+    if (Number.isNaN(parsedSpeed)) return;
+
     setMaxSpeed(parsedSpeed);
 
     if (isNaN(parsedSpeed) || parsedSpeed <= 0) {
@@ -98,8 +112,8 @@ export default function App() {
       return;
     }
 
-    setMaxSpeed(parsedSpeed); // Update the maximum speed
-    setInputSpeed(""); // Clear the input field
+    setMaxSpeed(parsedSpeed);
+    setInputSpeed("");
   };
 
   return (
@@ -116,35 +130,74 @@ export default function App() {
               { color: tracking ? "blue" : maxSpeedReached ? "red" : "green" },
             ]}
           >
-            {speed} kph
+            {speed} km/h
           </Text>
 
-          <Button
-            title={tracking ? "Stop Tracking" : "Start Tracking"}
-            onPress={() => setTracking(!tracking)}
-            color={tracking ? "red" : "blue"}
-          />
-          <Text style={styles.maxSpeedWarntitle}>Max Speed Reached!!</Text>
+          <View style={styles.btnContainer}>
+            {!pause && (
+              <Pressable
+                onPress={() => setTracking(!tracking)}
+                style={[
+                  styles.button,
+                  { backgroundColor: tracking ? "red" : "blue" },
+                ]}
+              >
+                <Text style={styles.buttonText}>
+                  {tracking ? "Stop Tracking" : "Start Tracking"}
+                </Text>
+              </Pressable>
+            )}
+            <View style={styles.gap} />
+            {tracking && (
+              <Pressable
+                onPress={() => setPause(!pause)}
+                style={[styles.button, { backgroundColor: "#9E9EFF" }]}
+              >
+                <Text style={styles.pauseButtonText}>
+                  {!pause ? "Pause" : "Continue"}
+                </Text>
+              </Pressable>
+            )}
+          </View>
+          {maxSpeedReached && (
+            <Text style={styles.maxSpeedWarntitle}>Max Speed Reached !</Text>
+          )}
         </View>
 
         <View style={styles.bottomSection}>
           <Text style={styles.subTitle}>Set maximum speed</Text>
-          <Text style={styles.speed}>Max Speed: {maxSpeed} kph</Text>
-          <TextInput
-            style={styles.input}
-            value={inputSpeed}
-            onChangeText={setInputSpeed}
-            keyboardType="numeric"
-            placeholder="Enter speed"
-            maxLength={3}
-          />
-          <View style={styles.saveMaxSpeedTextBtn}>
-            <Button
-              disabled={inputSpeed.length === 0}
-              title="Set Speed"
-              onPress={handleSetSpeed}
-              color="blue"
-            />
+          <Text style={styles.speed}>Max Speed: {maxSpeed} km/h</Text>
+          <View style={styles.maxSpeedActionContainer}>
+            {/* Input with Save Button */}
+            <View style={styles.inputWrapper}>
+              <TextInput
+                keyboardType="numeric"
+                value={inputSpeed}
+                style={styles.input}
+                placeholder="Enter speed"
+                onChangeText={setInputSpeed}
+                maxLength={3}
+              />
+              <TouchableOpacity
+                disabled={inputSpeed.length === 0}
+                onPress={() => handleSetSpeed()}
+                style={styles.saveMaxSpeedButton}
+              >
+                <Text style={styles.saveText}>Save</Text>
+              </TouchableOpacity>
+            </View>
+
+            {/* Microphone Icon */}
+            <TouchableOpacity
+              onPress={() => setListening(!listening)}
+              style={styles.microphoneButton}
+            >
+              {!listening ? (
+                <Icon name="microphone" size={20} color="#fff" />
+              ) : (
+                <Icon name="ellipsis-h" size={20} color="#fff" />
+              )}
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -156,68 +209,123 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 35,
-    // justifyContent: "center",
-    // alignItems: "center",
     backgroundColor: "#fff",
   },
   topSection: {
-    flex: 1, // Takes half the height of the screen
+    flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
   },
   bottomSection: {
-    flex: 1, // Takes the other half
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
   title: {
     fontSize: 28,
     fontWeight: "bold",
-    marginBottom: 10,
+    marginBottom: 5,
   },
   subTitle: {
     fontSize: 15,
     fontWeight: "light",
-    marginBottom: 20,
+    marginBottom: 15,
   },
   subTitleII: {
     fontSize: 10,
     fontWeight: "light",
     marginBottom: 20,
   },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 4,
+  },
+  gap: {
+    width: 10,
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    backgroundColor: "blue",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  pauseButtonText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
   speed: {
-    fontSize: 30,
+    fontSize: 26,
     color: "green",
     marginBottom: 30,
   },
   speedTracked: {
     fontSize: 48,
     color: "blue",
-    marginBottom: 30,
+    marginTop: 5,
+    marginBottom: 15,
   },
   maxSpeedWarntitle: {
-    fontSize: 38,
-    paddingTop: 40,
+    fontSize: 24,
+    paddingTop: 25,
     color: "red",
   },
   saveMaxSpeedTextBtn: {
     paddingHorizontal: 10,
   },
-  inputLabel: {
-    fontSize: 18,
-    marginTop: 10,
-    marginBottom: 10,
-    color: "gray",
+
+  maxSpeedActionContainer: {
+    flexDirection: "row",
+    alignItems: "center", // Ensures all elements are vertically aligned
+    paddingHorizontal: 10,
+    gap: 10,
+  },
+  inputWrapper: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    height: 50,
+    paddingHorizontal: 0,
   },
   input: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-    width: "80%",
-    textAlign: "center",
+    flex: 1,
+    fontSize: 16,
+    paddingLeft: 20,
+    paddingVertical: 0,
+  },
+  saveMaxSpeedButton: {
+    backgroundColor: "#3b5998",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 12,
+    height: "100%",
+    borderRadius: 8,
+    marginLeft: 10,
+  },
+  saveText: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  microphoneButton: {
+    backgroundColor: "#3b5998",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    height: 50,
+    width: 50,
   },
 });
